2b9365caa3a7a98be49e728bf9a2ddc650abb76d
James Honea <jameshonea@gmail.com>
2019-05-12 15:54:16 -0400

    imported json, added loop

--- weatherwidget.py
+++ weatherwidget.py
@@ -1,6 +1,20 @@
 import requests
+import json
+import time
 
-response = requests.get("https://api.darksky.net/forecast/8c4e31711730f9577556ad3878ae1fd0/39.855955, -86.338426")
-status_code = response.status_code
 
-print(status_code)
+loop = True
+while loop == True:
+    response = requests.get("https://api.darksky.net/forecast/8c4e31711730f9577556ad3878ae1fd0/39.855955, -86.338426")
+    status_code = response.status_code
+
+    json_data = response.json()
+    print(json_data)
+
+    print(json_data['currently'])
+
+    print(" ")
+
+    print(json_data['daily']['summary'])
+
+    time.sleep(300)
